04962008d92212442ec88975685285f91bf66c5f
Kamil <l3szcz@gmail.com>
2020-04-27 20:32:19 +0200

    Add state machine

---
+++ src/workDaysMachine.js
@@ -0,0 +1,65 @@
+import { Machine, assign } from "xstate";
+import {
+  differenceInBusinessDays as dbd,
+  addBusinessDays as abd,
+  format,
+} from "date-fns";
+
+const initialState = {
+  workDays: "",
+  dateStart: "",
+  dateEnd: "",
+};
+
+export const workDaysMachine = Machine({
+  id: "workDaysMachine",
+  initial: "active",
+  context: {
+    ...initialState,
+  },
+  states: {
+    active: {
+      on: {
+        DATE_START: {
+          actions: assign((context, event) => ({
+            ...context,
+            dateStart: event.value,
+            workDays: context.dateEnd
+              ? dbd(new Date(context.dateEnd), new Date(event.value))
+              : context.workDays,
+            dateEnd: context.workDays
+              ? format(
+                  abd(new Date(event.value), context.workDays),
+                  "yyyy-MM-dd"
+                )
+              : context.dateEnd,
+          })),
+        },
+        DATE_END: {
+          actions: assign((context, event) => ({
+            ...context,
+            dateEnd: event.value,
+            workDays: context.dateStart
+              ? dbd(new Date(event.value), new Date(context.dateStart))
+              : context.workDays,
+          })),
+        },
+        WORK_DAYS: {
+          actions: assign((context, event) => ({
+            ...context,
+            workDays: event.value,
+            dateEnd: context.dateStart
+              ? format(
+                  abd(new Date(context.dateStart), event.value),
+                  "yyyy-MM-dd"
+                )
+              : context.dateEnd,
+          })),
+        },
+        CLEAR: {
+          actions: assign({ ...initialState }),
+        },
+      },
+    },
+  },
+});
